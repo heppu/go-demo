1f301fb004fec3c1eec8c546aef783f853b13c3c
Henri Koski <henri.t.koski@gmail.com>
2018-02-28 11:18:35 +0200

    Fix data race in SliceStorage

--- api/slice_storage.go
+++ api/slice_storage.go
@@ -2,27 +2,36 @@ package api
 
 import (
 	"fmt"
+	"sync"
 
 	uuid "github.com/satori/go.uuid"
 )
 
 type SliceStorage struct {
-	users []User
+	userMu *sync.RWMutex
+	users  []User
 }
 
 func NewSliceStorage() *SliceStorage {
 	return &SliceStorage{
-		users: []User{},
+		userMu: &sync.RWMutex{},
+		users:  []User{},
 	}
 }
 
 func (ss *SliceStorage) AddUser(user User) (User, error) {
+	ss.userMu.Lock()
+	defer ss.userMu.Unlock()
+
 	user.ID = uuid.NewV4().String()
 	ss.users = append(ss.users, user)
 	return user, nil
 }
 
 func (ss *SliceStorage) GetUser(id string) (User, error) {
+	ss.userMu.RLock()
+	defer ss.userMu.RUnlock()
+
 	for _, user := range ss.users {
 		if user.ID == id {
 			return user, nil
@@ -33,6 +42,9 @@ func (ss *SliceStorage) GetUser(id string) (User, error) {
 }
 
 func (ss *SliceStorage) DeleteUser(id string) error {
+	ss.userMu.Lock()
+	defer ss.userMu.Unlock()
+
 	for i, user := range ss.users {
 		if user.ID == id {
 			ss.users = append(ss.users[:i], ss.users[i+1:]...)
